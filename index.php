8c0a003e92c34c9a94d87507e9c938b7dddc4621
Nitzan Hermon <x@editbyedit.co.uk>
2011-08-18 11:31:54 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<title>Blog by Edit</title>
+		<link rel="stylesheet" href="style.css" type="text/css">
+		
+	</head>
+	<body>
+	<div id="wrapper" class="column4">
+	<header>
+	
+		<h1 class="logo">Edit Feed</h1>
+		<p><i>Running on <a href="http://appbyedit.co.uk">Edit iPhone App</a> &bull; <a href="http://twitter.com/editbyedit">@editbyedit</a> &bull; <a href="mailto:x@byed.it">Contact</a></i></p>
+			
+	</header>	
+		<section>
+		<?php 
+    $xml = simplexml_load_file('http://appbyedit.co.uk/{USER ID}/feed');
+	
+	foreach ($xml->channel->children() as $i) {
+       
+       echo "<article class=''>", $i->description , "</article>";
+
+       
+    }
+
+		?>
+
+</section>
+	</div><!-- wrapper -->
+
+	</body>
+</html>
