7a43a0974d70cca29acb8af80107f357db94918a
Dai Hovey <dai@14lox.com>
2012-03-21 07:01:35 +1100

    Added Config section Added <hgroup> Added HTML5 doctype and charset Added titles to links Added title from XML to each item

--- index.php
+++ index.php
@@ -1,32 +1,48 @@
-<html>
+<?php
+	/*
+	  
+	  CONFIG - CHANGE THESE SETTINGS TO MATCH YOUR OWN
+	  
+	*/
+	   
+	$title = "My Blog, running on Edit app";
+	$edit_account = "0e2974";
+	$twitter_username = "14lox";
+	$email_address = "dai@14lox.com";
+	
+	/* END OF CONFIG */
+	
+	
+?>
+
+<!doctype html>
 	<head>
-		<title>Blog by Edit</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title><?php echo $title ?></title>
 		<link rel="stylesheet" href="style.css" type="text/css">
 		
 	</head>
 	<body>
-	<div id="wrapper" class="column4">
+	<div id="wrapper">
 	<header>
-	
-		<h1 class="logo">Edit Feed</h1>
-		<p><i>Running on <a href="http://appbyedit.co.uk">Edit iPhone App</a> &bull; <a href="http://twitter.com/TWITTER">@TWITTER</a> &bull; <a href="mailto:EMAIL">EMAIL</a></i></p>
-			
+		<hgroup>
+		<h1 class="logo"><?php echo $header ?></h1>
+		<p><i>Running on <a href="http://app.byed.it" title="http://app.byed.it" target="_blank">Edit iPhone App</a> &bull; <a href="http://twitter.com/<?php echo $twitter_username ?>" target="_blank" title="http://twitter.com/<?php echo $twitter_username ?>">@<?php echo $twitter_username ?></a> &bull; <a href="mailto:<?php echo $email_address ?>" title="<?php echo $email_address ?>""><?php echo $email_address ?></a></i></p>
+		</hgroup>	
 	</header>	
 		<section>
 		<?php 
-    $xml = simplexml_load_file('http://appbyedit.co.uk/{USER ID}/feed');
+			$xml = simplexml_load_file('http://app.byed.it/'.$edit_account.'/feed');
 	
-	foreach ($xml->channel->children() as $i) {
-       
-       echo "<article class=''>", $i->description , "</article>";
-
-       
-    }
-
+			foreach ($xml->channel->children() as $i)
+			{
+				echo "<h2>", $i->title , "</h2>";
+				echo "<article class=''>", $i->description , "</article>";
+			}
 		?>
 
-</section>
-	</div><!-- wrapper -->
-
+		</section>
+	</div><!-- wrapper -->	
 	</body>
 </html>
